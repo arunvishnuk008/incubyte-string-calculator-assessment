49139f1fd51a05c180a20cd8d1cc42e605a16ca6
Arun Vishnu K <arunvishnuk008@gmail.com>
2025-10-17 11:20:34 +0530

    multiple number addition now passing with support for custom delimiters

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,14 +5,33 @@ public class StringCalculator {
     public int add(String numbers) {
         int result = 0;
         if (numbers != null && !numbers.isBlank()) {
-            result = Arrays.stream(numbers.split(","))
+            // extract the delimiter if it is present
+            String delimiter = ",";
+            String numbersLine = numbers;
+            int endIndex = 0;
+            if (numbers.startsWith("//")) {
+                boolean notFound = true;
+                int index = 0;
+                while (notFound) {
+                    if (numbers.charAt(index) == '\n') {
+                        endIndex = index;
+                        notFound = false;
+                    }
+                    index += 1;
+                }
+                // we found index, construct the delimiter
+                delimiter = "\\" + numbers.substring(2, endIndex);
+                numbersLine = numbers.substring(endIndex);
+            }
+            result = Arrays.stream(numbersLine.split(delimiter))
                     .map(str -> str.replace("\n", ""))
-                    .filter( str -> !str.isBlank() )
+                    .filter(str -> !str.isBlank())
                     .map(Integer::parseInt)
                     .reduce(Integer::sum).orElse(0);
         }
         return result;
     }
+
 
     public static void main(String[] args) {
 
--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -52,4 +52,16 @@ public class StringCalculatorTests {
         int actual = stringCalculator.add(input);
         assertEquals(actual, Integer.parseInt(expected));
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//*\n6*6',12",
+            "'//#\n3#4\n#\n#5',12",
+            "'//&\n7&8&0&1&2&\n&&&\n&1',19",
+            "'//,\n0,\n,\n,0,1,,2,,,,5,,,,7,10',25"
+    })
+    void add_returns_result_whenMultipleNumbersAndDifferentDelimiterInArgument(String input, String expected) {
+        int actual = stringCalculator.add(input);
+        assertEquals(actual, Integer.parseInt(expected));
+    }
 }
